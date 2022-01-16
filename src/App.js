c394cd79eff90662b1936dd5c2b859c0bcd30d5b
Andrii Maistrenko <maystrenko22a@gmail.com>
2022-01-16 22:07:16 +0200

    report.page.markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+
+
+import ReportPage from "./pages/ReportPage/ReportPage";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+        <ReportPage />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/component/CategoryList/categoriesIcons.js
@@ -0,0 +1,47 @@
+import sprite from '../../images/sprite.svg';
+export const categories = [
+  {
+    name: 'продукты',
+    svg: `${sprite}#products`,
+  },
+  {
+    name: 'алкоголь',
+    svg: `${sprite}#cocktail`,
+  },
+  {
+    name: 'развлечения',
+    svg: `${sprite}#entertainment`,
+  },
+  {
+    name: 'здоровье',
+    svg: `${sprite}#health`,
+  },
+  {
+    name: 'транспорт',
+    svg: `${sprite}#car`,
+  },
+  {
+    name: 'все для дома',
+    svg: `${sprite}#home`,
+  },
+  {
+    name: 'техника',
+    svg: `${sprite}#tools`,
+  },
+  {
+    name: 'комуналка, связь',
+    svg: `${sprite}#bills`,
+  },
+  {
+    name: 'спорт, хобби',
+    svg: `${sprite}#hobby`,
+  },
+  {
+    name: 'образование',
+    svg: `${sprite}#education`,
+  },
+  {
+    name: 'прочее',
+    svg: `${sprite}#other`,
+  },
+];
