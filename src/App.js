9c6938090c0938eca5944a2f7d7dd75b5e882414
spelui <vlad.spelui@gmail.com>
2022-01-17 20:00:36 +0200

    bg

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
           {/*<OnLoader />*/}
           <Header />
           <AuthPage />
-          {/* <ReportPage /> */}
+          <ReportPage />
         </>
       )}
       <CostIncome />
